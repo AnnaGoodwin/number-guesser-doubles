225f966d6e671c3ba3dce1b08ba09e44ed4381a7
“Foster <fosterataylor@yahoo.com>
2019-07-03 16:30:24 -0600

    Changed numeric input type and added border to header

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+var numberInput = document.querySelector('.input')
+
+numberInput.addEventListener('click', function () {
+  var color = colorInput.value;
+  console.log(color);
+};
+
 var h1 = document.querySelector('h1');
 var button = document.querySelector('.guesser-guess-form-button');
 
